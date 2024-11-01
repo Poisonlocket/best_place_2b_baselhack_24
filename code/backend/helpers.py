945292ad5cee327642552215d06412a6c8ce6149
Leonard V. Karsunky <lkarsunky3@gatech.edu>
2024-11-02 00:46:57 +0100

    Changed upload_image to upload_images

--- code/backend/helpers.py
+++ code/backend/helpers.py
@@ -2,24 +2,21 @@ import os
 from flask import request
 
 IMAGE_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def upload_image():
-    if 'file' not in request.files:
-            print('No file part')
-            print(request.files)
-            return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        print('No selected file')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = file.filename 
-        filepath = os.path.join(IMAGE_FOLDER, filename)
-        file.save(filepath)
-        return f"File uploaded successfully to {filepath}"
-    else:
-        return "Invalid file type"
+def upload_images():
+    images = request.files.getlist('images')
+
+    for image in images:
+        if image and allowed_file(image.filename):
+            filename = image.filename 
+            filepath = os.path.join(IMAGE_FOLDER, filename)
+            image.save(filepath)
+        else:
+            raise TypeError("Invalid file type")
+        
+def upload_audio():
+    audio = request.files.get('audio')
