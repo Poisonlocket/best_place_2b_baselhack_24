f3fdce396762592583d80075e2ce9e319b5e26e8
Poisonlocket <loribono@gmx.ch>
2024-11-01 22:07:23 +0100

    added 418 Endpoint

---
+++ code/backend/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+
+@app.get("/app")
+def hello():
+    return "henlo"
+
+
+@app.get("/418")
+def teapot():
+    art = r"""
+            .------.____
+         .-'       \ ___)
+      .-'         \\\
+   .-'        ___  \\)
+.-'          /  (\  |)
+         __  \  ( | |
+        /  \  \__'| |
+       /    \____).-'
+     .'       /   |
+    /     .  /    |
+  .'     / \/     |
+ /      /   \     |
+       /    /    _|_
+       \   /    /\ /\
+        \ /    /__v__\
+         '    |       |
+              |     .#|
+              |#.  .##|
+              |#######|
+              |#######|    
+"""
+
+    return Response(art, mimetype="text/plain")
+
+
+app.run()
