509b41b0b295e745403dfa2323ca0ba9d4230264
Leonard V. Karsunky <lkarsunky3@gatech.edu>
2024-11-02 00:55:25 +0100

    Added upload_audio() and changed upload_images()

--- code/backend/helpers.py
+++ code/backend/helpers.py
@@ -1,25 +1,35 @@
 import os
 from flask import request
 
-IMAGE_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+IMAGE_FOLDER = 'uploads/images'
+ALLOWED_IMAGE_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+AUDIO_FOLDER = 'uploads/audio'
+ALLOWED_AUDIO_EXTENSIONS = {'mp3', 'm4a', 'wav'}
 
-def upload_image():
-    if 'file' not in request.files:
-            print('No file part')
-            print(request.files)
-            return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        print('No selected file')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = file.filename 
-        filepath = os.path.join(IMAGE_FOLDER, filename)
-        file.save(filepath)
-        return f"File uploaded successfully to {filepath}"
+def allowed_image_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_IMAGE_EXTENSIONS
+
+def allowed_audio_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_AUDIO_EXTENSIONS
+
+def upload_images():
+    images = request.files.getlist('images')
+
+    for image in images:
+        if image and allowed_image_file(image.filename):
+            filename = image.filename 
+            filepath = os.path.join(IMAGE_FOLDER, filename)
+            image.save(filepath)
+        else:
+            raise TypeError("Invalid file type for image file: ", image.filename)
+        
+def upload_audio():
+    audio = request.files.get('audio')
+
+    if audio and allowed_audio_file(audio.filename):
+        filename = audio.filename
+        filepath = os.path.join(AUDIO_FOLDER, filename)
+        audio.save(filepath)
     else:
-        return "Invalid file type"
+        raise TypeError("Invalid file type for audio file: ", audio.filename)
