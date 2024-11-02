6a4f8ac5c1b3397acb9ced58f20daeb3884687e9
Jonas Passweg <jpassweg@student.ethz.ch>
2024-11-02 01:49:58 +0100

    feat: add test image send to backend

--- code/2b-a-frontend-dev/src/pages/Test.js
+++ code/2b-a-frontend-dev/src/pages/Test.js
@@ -1,11 +1,47 @@
-import React from "react";
+import React from 'react';
+import { Button } from "flowbite-react"
+import axios from 'axios';
+import FormData from 'form-data';
+import imageFile from '../assets/test1.jpg';
 
-const Test = () => {
-    return (
-        <div>
-            <h1>Welcome to Test</h1>
-        </div>
-    );
-};
+
+
+function Test() {
+  async function getData() {
+    try {
+      const response = await fetch(imageFile);
+      const fileBlob = await response.blob();
+
+      let data = new FormData();
+      data.append('file', fileBlob, "hello darkness my old friend");
+      return data
+    } catch(err) {
+      console.error(err)
+    }
+  }
+
+  async function storeImage() {
+    const data = await getData()
+    axios.post("https://localhost:5000", data, {
+      headers: {
+        'accept': 'application/json',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+      }
+    })
+      .then((response) => {
+        // TODO: add response to state here
+        console.log(response)
+      }).catch((error) => {
+        console.error(error)
+      });
+  }
+
+  return (
+    <div>
+      <Button onClick={storeImage} >Save</Button>
+    </div>
+  );
+}
 
 export default Test;
