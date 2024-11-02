1a46ee139d4e6cca57d688eafbac59859b76ca95
Tamalera <florence.meier@alumni.ethz.ch>
2024-11-02 11:55:25 +0100

    Formatting and E&H image

--- code/ai/vision_testy.py
+++ code/ai/vision_testy.py
@@ -1,31 +1,32 @@
-from openai import OpenAI
-from dotenv import load_dotenv
 import os
 
+from dotenv import load_dotenv
+from openai import OpenAI
+
 load_dotenv()  # Load environment variables from .env file
-api_key = os.getenv('OPENAI_API_KEY')
+api_key = os.getenv("OPENAI_API_KEY")
 
 client = OpenAI()
 
 # this will make the output extra cute: Describe it as if I were 5 years old.
 
 response = client.chat.completions.create(
-  model="gpt-4o-mini",
-  messages=[
-    {
-      "role": "user",
-      "content": [
-        {"type": "text", "text": "What’s in this image?"},
+    model="gpt-4o-mini",
+    messages=[
         {
-          "type": "image_url",
-          "image_url": {
-            "url": "https://assets.alliedelec.com/f_auto,q_auto/73689460.jpg",
-          },
-        },
-      ],
-    }
-  ],
-  max_tokens=100,
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "What’s in this image?"},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": "https://www.endress.com/__image/a/9511035/k/966cb24d9fa10ad1514dad54e51fae2b68afdfeb/ar/1-1/w/344/t/jpg/b/ffffff/n/true/fn/Bundle%2001_Template_71481757_CDP50%20Panel%20for%20free%20Chlorine.jpg",
+                    },
+                },
+            ],
+        }
+    ],
+    max_tokens=100,
 )
 
 print(response.choices[0].message.content)
