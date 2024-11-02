da0d8b81b4522c2a880ab82bcde11f85f9687bb6
fabandrade88 <fabricio.solutions13@gmail.com>
2024-11-02 17:27:53 +0100

    homepage and buttons

--- code/2b-a-frontend-dev/src/pages/index.js
+++ code/2b-a-frontend-dev/src/pages/index.js
@@ -1,10 +1,9 @@
 import React from "react";
+import HomeComponent from "../components/homecomponent";
 
 const Home = () => {
     return (
-        <div>
-            <h1>Welcome to Home</h1>
-        </div>
+        <HomeComponent />
     );
 };
 
