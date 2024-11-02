a563f550a91932986fc8582297419a6a061c1164
Tamalera <florence.meier@alumni.ethz.ch>
2024-11-02 13:21:49 +0100

    Add static image instead of URL

--- code/ai/vision_testy.py
+++ code/ai/vision_testy.py
@@ -1,3 +1,4 @@
+import base64
 import os
 
 from dotenv import load_dotenv
@@ -6,27 +7,39 @@ from openai import OpenAI
 load_dotenv()  # Load environment variables from .env file
 api_key = os.getenv("OPENAI_API_KEY")
 
+
 client = OpenAI()
 
-# this will make the output extra cute: Describe it as if I were 5 years old.
+## TODO: Use the image from the FE
+image_path = "../../assets/test_data_pen_1.jpg"
 
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+base64_image = encode_image(image_path)
+
+## Multiple images also possible; see https://platform.openai.com/docs/guides/vision#multiple-image-inputs
 response = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
+  model="gpt-4o-mini",
+  messages=[
+    {
+      "role": "user",
+      "content": [
         {
-            "role": "user",
-            "content": [
-                {"type": "text", "text": "What’s in this image?"},
-                {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": "https://www.endress.com/__image/a/9511035/k/966cb24d9fa10ad1514dad54e51fae2b68afdfeb/ar/1-1/w/344/t/jpg/b/ffffff/n/true/fn/Bundle%2001_Template_71481757_CDP50%20Panel%20for%20free%20Chlorine.jpg",
-                    },
-                },
-            ],
-        }
-    ],
-    max_tokens=100,
+          "type": "text",
+          "text": "What is in this image?",
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url":  f"data:image/jpg;base64,{base64_image}"
+          },
+        },
+      ],
+    }
+  ],
+  max_tokens=100
 )
 
 print(response.choices[0].message.content)
