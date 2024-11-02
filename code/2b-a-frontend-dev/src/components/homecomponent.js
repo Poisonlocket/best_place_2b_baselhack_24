14c034b945ae6e2ff9e263c86635ee90ee3eeb6d
fabandrade88 <fabricio.solutions13@gmail.com>
2024-11-02 19:25:58 +0100

    home

--- code/2b-a-frontend-dev/src/components/homecomponent.js
+++ code/2b-a-frontend-dev/src/components/homecomponent.js
@@ -5,8 +5,8 @@ import peopleImage from '../assets/people.png'; // Import the image
 
 const HomeComponent = () => {
     return (
-        <div className="flex items-center justify-start p-4 h-[80vh]">
-            <div className="flex-1 flex-shrink-0 mr-[5px]">
+        <div className="flex flex-col md:flex-row items-center justify-start p-4 h-[80vh]"> {/* Use flex-col for small screens */}
+            <div className="people flex-1 flex-shrink-0 mb-4 md:mb-0 md:mr-[5px]"> {/* Add margin bottom for small screens */}
                 <img 
                     src={peopleImage} 
                     alt="Developers working with Open API"
@@ -17,11 +17,6 @@ const HomeComponent = () => {
                 <h1 className="text-6xl font-bold text-center">SmartGuide</h1>
                 <h2 className="text-xl text-gray-600 px-1.5 text-center">Simple creation. Simple Use!</h2>
                 <div className="flex space-x-4 mt-4"> {/* Add space between buttons and margin on top */}
-                    <Link to="/view">
-                        <button type="button" className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-                            Get Started
-                        </button>
-                    </Link>
                     <Link to="/edit">
                         <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
                             New Guide
@@ -39,6 +34,7 @@ const HomeComponent = () => {
 }
 
 export default HomeComponent;
+
 
 
 
