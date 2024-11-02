b142f256cf1f42883be8d0052a5cda183de58a63
Jonas Passweg <jpassweg@student.ethz.ch>
2024-11-02 13:16:59 +0100

    chore: specing

--- code/2b-a-frontend-dev/src/components/FileWithID.js
+++ code/2b-a-frontend-dev/src/components/FileWithID.js
@@ -1,9 +1,9 @@
 class FileWithID {
-	constructor(fileContent, step_number, file_number, isImage){
+	constructor(fileContent, step_number, file_number, isImage) {
     	this.fileContent = fileContent;
       this.step_number = step_number;
       this.file_number = file_number;
-      this.isImage = isImage
+      this.isImage = isImage;
     }
   	displayInfo(){
     	return "" + this.step_number + "." + this.image_number + (this.isImage ? ".jpg" : ".ogg");
