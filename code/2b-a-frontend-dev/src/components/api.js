6bc6e6b6a3ea9710885855e196efde63faa9c43a
Jonas Passweg <jpassweg@student.ethz.ch>
2024-11-02 18:56:25 +0100

    feat: implement saveAndProcessGuide

--- code/2b-a-frontend-dev/src/components/api.js
+++ code/2b-a-frontend-dev/src/components/api.js
@@ -1,16 +1,51 @@
 // api.js
-export const saveAndProcessGuide = async (guide) => {
+export const saveAndProcessGuide = async ({ guide }) => {
     try {
         // Simulate a delay of 1 second
-        await new Promise(resolve => setTimeout(resolve, 1000));
-
+        //await new Promise(resolve => setTimeout(resolve, 1000));
+        let data = guideToData(guide)
+        let new_uuid = await storeImages(data);
         // Mock response data
-        const data = { message: "Guide processed successfully", guide };
-
-        console.log("Fake API response:", data);
-        return data;
+        // const response = { message: "Guide processed successfully", guide};
+        // console.log("Fake API response:", response);
+        let new_guide = (prevGuide => 
+            new Guide(prevGuide.name, new_uuid, prevGuide.startImage, 
+                prevGuide.description, prevGuide.section));
+        return new_guide;
     } catch (error) {
         console.error("Failed to process guide:", error);
         throw error;
     }
 };
+
+function guideToData(guides) {
+    let data = new FormData();
+    for (let i = 0; i < guides.sections.length; i++) {
+        let currSection = guides.sections[i];
+        for (let j = 0; j < currSection.images.length; j++) {
+            data.append('awesome_files', currSections.images[j], "" + guides.uuid 
+                + "." + i + "." + j + ".jpg");
+        }
+        if(currSection.recording) {
+            data.append('awesome_files', currSections.images[j], "" + guides.uuid 
+                + "." + i + "." + currSection.images.length + ".ogg");
+        }
+    }
+    return data;
+}
+
+async function storeImages(data) {
+    axios.post("https://localhost:5000/upload", data, {
+        headers: {
+            'accept': 'application/json',
+            'Content-Encoding': 'gzip',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
+        }
+    }).then((response) => {
+        console.log(response.data.guide_id)
+        return response.data.guide_id;
+    }).catch((error) => {
+        console.error(error)
+    });
+}
