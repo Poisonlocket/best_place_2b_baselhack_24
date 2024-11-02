c48b8694540551afd41898cdea4dc501f96bc6e2
Daniel Friedrich <friedrich@nanosurf.com>
2024-11-02 20:08:35 +0100

    Improve mobile design

--- code/2b-a-frontend-dev/src/pages/View.js
+++ code/2b-a-frontend-dev/src/pages/View.js
@@ -3,9 +3,9 @@ import CarrouselView from "../components/Carrousel";
 
 const View = () => {
     return (
-        <div className="w-full min-h-screen p-8">
-            <h1 className="text-2xl font-bold mb-6">SmartGuide:</h1>
-            <div className="flex-grow">
+        <div className="w-full min-h-screen p-4 bg-gray-100 flex flex-col items-center">
+            <h1 className="text-3xl font-bold mb-4 text-center">SmartGuide</h1>
+            <div className="w-full max-w-4xl flex-grow">
                 <CarrouselView />
             </div>
         </div>
